ff5b8f24cebf5d31f21bb9dc71d9bb028b92d353
alexkingorg <alexkingorg@e2bd1680-98a1-11dd-bd51-a78b85c98fae>
2009-01-13 08:17:51 +0000

    testing the ability to remove some standard theme files

--- experimental/text/functions/templates.php
+++ experimental/text/functions/templates.php
@@ -17,6 +17,74 @@
 
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 
+/*
+// 	} else if ( is_404() && $template = get_404_template() ) {
+// 		include($template);
+// 		return;
+// 	} else if ( is_search() && $template = get_search_template() ) {
+// 		include($template);
+// 		return;
+	} else if ( is_tax() && $template = get_taxonomy_template()) {
+		include($template);
+		return;
+// 	} else if ( is_home() && $template = get_home_template() ) {
+// 		include($template);
+// 		return;
+	} else if ( is_attachment() && $template = get_attachment_template() ) {
+		remove_filter('the_content', 'prepend_attachment');
+		include($template);
+		return;
+// 	} else if ( is_single() && $template = get_single_template() ) {
+// 		include($template);
+// 		return;
+	} else if ( is_page() && $template = get_page_template() ) {
+		include($template);
+		return;
+// 	} else if ( is_category() && $template = get_category_template()) {
+// 		include($template);
+// 		return;
+// 	} else if ( is_tag() && $template = get_tag_template()) {
+// 		include($template);
+// 		return;
+// 	} else if ( is_author() && $template = get_author_template() ) {
+// 		include($template);
+// 		return;
+// 	} else if ( is_date() && $template = get_date_template() ) {
+// 		include($template);
+// 		return;
+// 	} else if ( is_archive() && $template = get_archive_template() ) {
+// 		include($template);
+// 		return;
+// 	} else if ( is_comments_popup() && $template = get_comments_popup_template() ) {
+// 		include($template);
+// 		return;
+// 	} else if ( is_paged() && $template = get_paged_template() ) {
+// 		include($template);
+// 		return;
+	} else if ( file_exists(TEMPLATEPATH . "/index.php") ) {
+		include(TEMPLATEPATH . "/index.php");
+		return;
+	}
+*/
+function cfct_posts_template($file) {
+	$file = cfct_choose_general_template('posts');
+	return $file;
+}
+add_filter('archive_template', 'cfct_posts_template');
+add_filter('author_template', 'cfct_posts_template');
+add_filter('category_template', 'cfct_posts_template');
+add_filter('home_template', 'cfct_posts_template');
+add_filter('search_template', 'cfct_posts_template');
+add_filter('tag_template', 'cfct_posts_template');
+add_filter('taxonomy_template', 'cfct_posts_template');
+
+function cfct_single_template($file) {
+	$file = cfct_choose_general_template('single');
+	return $file;
+}
+add_filter('single_template', 'cfct_single_template');
+
+
 function cfct_page($file = '') {
 	if (empty($file)) {
 		$file = cfct_default_file('pages');
